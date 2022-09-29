7cb68d1f2663eb17b905323565f9248ae9f884ab
luqmanito <109712257+luqmanito@users.noreply.github.com>
2022-09-29 20:32:30 +0700

    Update Task4-.js

--- Task4-.js
+++ Task4-.js
@@ -1,13 +1,13 @@
 const fetchAPI = fetch("https://jsonplaceholder.typicode.com/users");
 
 const promise = fetchAPI.then(res => {
-  return res.json()
+    return res.json()
 });
 
-promise.then(data => {
-    const showedData = data.map(data => data.name)
-    console.log(showedData)
-})
-.catch(err => {
-    console.error(err.message);
-})
+promise
+    .then(data => {
+        const showedData = data.map(data => data.name)
+        console.log(showedData)
+    })
+    .catch(err => console.error(err.message)
+    )
